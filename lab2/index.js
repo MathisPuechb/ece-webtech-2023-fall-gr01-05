48fa09d2f5f32dc9e321d696cda6dfe0080f7ab8
bouissou <bouissou.n@outlook.fr>
2023-10-04 14:39:26 +0200

    fix: reogranizing

--- lab2/index.js
+++ lab2/index.js
@@ -1,14 +1,37 @@
-// Import a module
-const http = require('http')
+// Define a string constant concatenating strings
+const content = '<!DOCTYPE html>' +
+'<html>' +
+'    <head>' +
+'        <meta charset="utf-8" />' +
+'        <title>ECE AST</title>' +
+'    </head>' + 
+'    <body>' +
+'       <p>Hello World!</p>' +
+'    </body>' +
+'</html>'
+ const url = require('url')
+ const qs = require('querystring')
 
-// Declare an http server
-http.createServer(function (req, res) {
+const serverHandle = function (req, res) {
 
-  // Write a response header
-  res.writeHead(200, {'Content-Type': 'text/plain'})
+  const route = url.parse(req.url)
+  const path = route.pathname 
+  const params = qs.parse(route.query)
+  const path = url.parse(req.url).pathname
+  console.log(path)
+  const queryParams =qs.parse(url.pars(req.url).query)
+  console.log(queryParams)
+  res.writeHead(200, {'Content-Type': 'text/html'})
+  if (path === '/hello' && 'name' in params) {
+    res.write('Hello ' + params['name'])
+  } else {
+    res.write('Hello anonymous')
+  }
+  
+  res.write(content)
+  res.end()
+}
 
-  // Write a response content
-  res.end('Hello World\n')
-
-// Start the server
-}).listen(8080)
+http
+.createServer(serverHandle)
+.listen(8080)
