ac1fd33154772094bd6d48d97e877ca03b833f25
vboxuser <vboxuser@Ubuntu.myguest.virtualbox.org>
2023-10-05 12:00:34 +0200

    feat: add new folders

---
+++ Lab3/routes/hello.js
@@ -0,0 +1,9 @@
+const express = require('express')
+const router = express.Router()
+
+router.get('/:name', (req, res) => {
+  console.log(req.params)
+  res.send("Hello " + req.params.name)
+})
+
+module.exports = router
