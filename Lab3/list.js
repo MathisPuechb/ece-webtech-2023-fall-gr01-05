2f6dd6a846b384f25c70fdb4056d8b16e9137006
vboxuser <vboxuser@Ubuntu.myguest.virtualbox.org>
2023-10-08 16:05:03 +0200

    feat: post v1

--- Lab3/list.js
+++ Lab3/list.js
@@ -8,6 +8,28 @@ app.get('/', (req, res) => {
   res.json(articles); 
 });
 
+
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
+});
+
+
+app.post('/articles', (req, res) => {
+  
+  const { title, content, date, author } = req.body;
+  
+
+  const nvarticle = {
+    id: '1235646', 
+    title: 'How to push an article',
+    content: 'Good content',
+    date:'08/10/2035',
+    author:'Us',
+  };
+ 
+  articles.articles.push(nvarticle);
+  console.log('Nouvel article ajouté :', newArticle);
+  res.status(201).json(nvarticle);
+
+  
 });
