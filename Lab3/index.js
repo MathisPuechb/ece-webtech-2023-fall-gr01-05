0fa4b1f0ff2faf3cf144ba8245d7ef730252a3e4
bouissou <bouissou.n@outlook.fr>
2023-10-11 12:13:44 +0200

    fix: renaming files

--- Lab3/index.js
+++ Lab3/index.js
@@ -1,14 +1,42 @@
-const express = require('express')
-const app = express()
-const home = require("./routes")
-const hello = require("./routes/hello")
+const express = require('express');
+const app = express();
+const port = 8080;
+const searchId = '123456789'
+const articles = require('./articles');
+const article = articles.articles.find( article => article.id === searchId)
+// returns an element of an array or `undefined`
 
-app.use('/', home);
-app.use('/hello', hello);
 
-app.set('port', 8080)
+app.get('/id', (req,res) => {
+  res.json(article);
+})
 
-app.listen(
-  app.get('port'),
-  () => console.log(`server listening on ${app.get('port')}`)
-)
+
+app.get('/', (req, res) => {
+  res.json(articles);
+});
+
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
+
+
+app.post('/articles', (req, res) => {
+ 
+  const { id, title, content, date, author } = req.body;
+ 
+
+  const nvarticle = {
+    id: '123564667',
+    title: 'How to push an article',
+    content: 'Good content',
+    date:'08/10/2035',
+    author:'Us',
+  };
+ 
+  articles.articles.push(nvarticle);
+  res.status(201).json(nvarticle);
+
+ 
+});
