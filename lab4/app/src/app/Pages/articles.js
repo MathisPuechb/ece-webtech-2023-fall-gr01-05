43f989035efb3df59181e56a9fd44dc5f8c661c9
bouissou <bouissou.n@outlook.fr>
2023-10-12 10:18:06 +0200

    feat: adding the skeleton of lab4

---
+++ lab4/app/src/app/Pages/articles.js
@@ -0,0 +1,14 @@
+import Header from '../components/Header';
+import Footer from '../components/Footer';
+
+const Articles = () => {
+  return (
+    <div>
+      <Header />
+      <h1>Articles Page</h1>
+      <Footer />
+    </div>
+  );
+};
+
+export default Articles;
