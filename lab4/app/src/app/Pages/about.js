d8d76e3ad4e9d407c43aafa605e40f0c59620f2f
bouissou <bouissou.n@outlook.fr>
2023-10-12 10:18:06 +0200

    feat: adding the skeleton of lab4

---
+++ lab4/app/src/app/Pages/about.js
@@ -0,0 +1,14 @@
+import Header from '../components/Header';
+import Footer from '../components/Footer';
+
+const About = () => {
+  return (
+    <div>
+      <Header />
+      <h1>About Page</h1>
+      <Footer />
+    </div>
+  );
+};
+
+export default About;
