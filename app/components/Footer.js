34a344500a95285933ccfac925ffa5645f2b82cb
MathisPuechb <vboxuser@Ubuntu.myguest.virtualbox.org>
2023-10-14 11:46:08 +0200

    feat: lab5 update

--- app/components/Footer.js
+++ app/components/Footer.js
@@ -1,12 +1,11 @@
 import React from 'react';
 
-
-export default function Header() {
-    return (
-        <footer>
-            <center>
-            <h1> Footer : miou miam</h1>
-            </center>
-        </footer>
-    )
+export default function Footer() {
+  return (
+    <footer className="bg-blue-500 py-4">
+      <div className="text-center">
+        <h1 className="text-4xl text-white font-bold">Footer: Miou Miam</h1>
+      </div>
+    </footer>
+  );
 }
