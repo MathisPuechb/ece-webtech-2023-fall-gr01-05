3025452342e3454fbae57dbd1af856a7ca637b3e
MathisPuechb <vboxuser@Ubuntu.myguest.virtualbox.org>
2023-10-14 12:20:59 +0200

    feat: custom class directive and style the layout

--- app/pages/index.js
+++ app/pages/index.js
@@ -3,9 +3,14 @@ import Layout from "../components/Layout";
 
 function Pages({ title, component }) {
   return (
-    <div>
-      <p className="text-2xl font-bold">{title}</p>
-      <p className="text-lg">{component}</p>
+    <div className="relative">      
+      {/* Right Rectangle */}
+      <div className="absolute top-0 right-0 w-16 h-full bg-blue-500"></div>
+
+      <div className="p-4">
+        <p className="text-2xl font-bold">{title}</p>
+        <p className="text-lg">{component}</p>
+      </div>
     </div>
   );
 }
