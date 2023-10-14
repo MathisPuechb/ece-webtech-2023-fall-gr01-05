ea398d8c3368e6f5775d1ce428478971669a5e11
MathisPuechb <vboxuser@Ubuntu.myguest.virtualbox.org>
2023-10-14 11:46:08 +0200

    feat: lab5 update

--- app/pages/index.js
+++ app/pages/index.js
@@ -1,27 +1,21 @@
-import React from "react"
-import Layout from "../components/Layout"
+import React from "react";
+import Layout from "../components/Layout";
 
-function Pages ({title, component}){
-
+function Pages({ title, component }) {
   return (
     <div>
-      <p>
-        {title}
-      </p>
-      <p>
-        {component}
-      </p>
-
+      <p className="text-2xl font-bold">{title}</p>
+      <p className="text-lg">{component}</p>
     </div>
   );
 }
 
-
-export default function homePage (){
-return (
-  <Layout>
-    {}
-    < Pages title= "home page" component="miam cat" /> 
-  </Layout>
-);
+export default function HomePage() {
+  return (
+    <Layout>
+      <div className="bg-gray-100 p-4">
+        <Pages title="Home Page" component="Welcome to our website" />
+      </div>
+    </Layout>
+  );
 }
