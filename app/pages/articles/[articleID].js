d09dfb66be9af83704e720119c91bbd0673fa671
nicolas <bouissou.n@outlook.fr>
2023-12-24 16:58:11 +0100

    feat: adding the comments fonctionality

--- app/pages/articles/[articleID].js
+++ app/pages/articles/[articleID].js
@@ -3,21 +3,25 @@ import React, { useEffect, useState } from "react";
 import { useRouter } from "next/router";
 import Layout from "../../components/Layout";
 import PostDetail from "../../components/articleDetails";
+import CommentSection from "../../components/comments"; 
 import { useSupabaseClient } from '@supabase/auth-helpers-react';
 
 const PostDetailPage = () => {
   const router = useRouter();
   const supabase = useSupabaseClient();
-  const postId = router.query; // Assuming you're using Next.js routing with query parameters
+  const { articleID } = router.query;
   const [post, setPost] = useState(null);
+  const [comments, setComments] = useState([]);
 
   useEffect(() => {
-    // Fetch the specific post from the database and update state
     const fetchPost = async () => {
       try {
-        console.log("uuid: ",postId);
-        const { data, error } = await supabase.from("posts").select("*").eq("id_article", postId.articleID).single();
-        console.log("data: ", data);
+        const { data, error } = await supabase
+          .from("posts")
+          .select("*")
+          .eq("id_article", articleID)
+          .single();
+
         if (error) {
           console.error("Error fetching post:", error.message);
         } else {
@@ -27,16 +31,35 @@ const PostDetailPage = () => {
         console.error("Error fetching post:", error.message);
       }
     };
-   
-    if (postId) {
+
+    const fetchComments = async () => {
+      try {
+        const { data, error } = await supabase
+          .from("comments")
+          .select("*")
+          .eq("post_id", articleID);
+
+        if (error) {
+          console.error("Error fetching comments:", error.message);
+        } else {
+          setComments(data);
+        }
+      } catch (error) {
+        console.error("Error fetching comments:", error.message);
+      }
+    };
+
+    if (articleID) {
       fetchPost();
+      fetchComments();
     }
-  }, [postId]);
+  }, [articleID]);
 
   return (
     <Layout>
       <div>
-        {post ? <PostDetail post={post} /> : <p>Loading...</p>}
+        {post ? <PostDetail post={post} /> : <p>Loading...</p>}    
+      <CommentSection comments={comments} postId={articleID} /> {/* Render CommentSection */}
       </div>
     </Layout>
   );
